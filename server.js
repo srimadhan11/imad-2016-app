aae0b8afe1b0cfb0458422d8c8b44467107b2c96
srimadhan11 <srimadhan11@gmail.com>
2016-10-05 07:27:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/:hello',function(req,res){
+    res.send(hello.toString());
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
