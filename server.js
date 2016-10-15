1e3726ddf5db961a9b3c3f2b03ecd8234104ca5b
srimadhan11 <srimadhan11@gmail.com>
2016-10-15 16:54:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/ui/main.js',function(req,res){
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  res.sendFile(path.join(__dirname, 'ui', 'new.html'));
 });
 
 
