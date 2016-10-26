e172c8b3c968de209713553793ee03bb5cc4fbc5
srimadhan11 <srimadhan11@gmail.com>
2016-10-26 12:05:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,8 +23,25 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'new.html'));
 });
 
+app.get('/chat/:string',function(req,res){
+    var str = getParameterByName('i');
+    res.send(str.toString());
+});
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
+
+function getParameterByName(name, url) {
+    if (!url) {
+        url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
