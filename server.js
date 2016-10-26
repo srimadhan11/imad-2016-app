e2dbed777274839e873aea8ba8c156d83c94516f
srimadhan11 <srimadhan11@gmail.com>
2016-10-26 22:22:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.get('/chat/prof',function(req,res){
             if(result.rows.length===0){
                 res.send("User does not exist");
             }else{
-                res.send(result.row+"$1",["hhhhhhhhhhhhhhhh"]);
+                res.send(result.row);
             }
         }
     });
