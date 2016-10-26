ffbb0edbebc6c8c7d3f3fc44dcf523b0b4c193f7
srimadhan11 <srimadhan11@gmail.com>
2016-10-26 12:12:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/:hello',function(req,res){
+app.get('/repeat/:hello',function(req,res){
     var hello=req.params.hello;
     res.send(hello.toString());
 });
