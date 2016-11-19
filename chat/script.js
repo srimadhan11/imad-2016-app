fd8196ed4f4732c6df3ad442f69ddcf65d822796
srimadhan11 <srimadhan11@gmail.com>
2016-11-19 14:04:52 +0530

    [imad-console] Updates chat/script.js

--- chat/script.js
+++ chat/script.js
@@ -2,64 +2,72 @@ var user;
 var password;
 var friendd;
 var flag=false;
-var url='http://srimadhan11.imad.hasura-app.io';
+var web='http://srimadhan11.imad.hasura-app.io';
+var local='http://localhost:8080';
+var url=local;
 
 function welcome() {
     document.body.innerHTML=`
     <div class="container">
-        <div align="center" font-family="san-serif">Welcome</div>
-        <input class="inp" id="email" type="email" placeholder="E-mail">
+        <div align="center" font-family="san-serif" class="text">Welcome</div>
+        <input class="inp" id="email" type="email" placeholder="Username">
         <input class="inp" id="pass" type="password" placeholder="Password">
         <button id="login" onclick="log()" class="btn-pr">Log In</button>
         <button id="signup" onclick="newac()" class="btn-sec">Sign In</button>
-    </div>
-    `;
+    </div>`;
     document.title='Welcome';
 }
 function newac() {
     document.body.innerHTML=`
     <div class="container">
-        <div align="center" font-family="san-serif">Welcome</div>
-        <input class="inp" id="newemail" type="email" placeholder="E-mail">
+        <div align="center" font-family="san-serif" class="text">Welcome</div>
+        <input class="inp" id="newemail" type="email" placeholder="Username">
         <input class="inp" id="newpass" type="password" placeholder="Password">
         <button onclick="create()" class="btn-pr">Create & Log In</button>
-    </div>
-    `;
+    </div>`;
     document.title='Sign Up';
 }
 function prof() {
     flag=true;
     document.body.innerHTML=`
-    <div style="display: flex">
-        <div id="friend1" style="width:30%;">
-            <div>
-                <input class="inp" id="friend" type="text" placeholder="Search Friends">
-                <button class="btn-pr" id="search" onclick="search()">Search</button>
-            </div>
-            <div id="fr">
+    <div align="center" font-family="san-serif" class="text">Chat</div>
+    <button class="btn-pr" style="float: right;" onclick="logout()">Logout</button>
+      <div class="container3">
+          <div id="friend1" style="
+      margin-top: 10px;
+      margin-bottom: 10px;
+      margin-left: 5px;
+      margin-right: 30px;
+  ">
+              <div>
+                  <input class="inp" id="friend" type="text" placeholder="Search Friends">
+                  <button class="btn-pr" id="search" onclick="search()">Search</button>
+              </div>
+  <br>
+              <div id="fr">
 
-            </div>
-        </div>
-        <div style="border-left:solid grey;"></div>
-        <div id="chat" style="width:70%">
-            <div style="height:90%" id="ch">
-
-            </div>
-            <div style="height:10%">
-                <input class="inp" id="msg" type="text" placeholder="Write a message">
-                <button class="btn-sec" id="send" onclick="send()">Send</button>
-            </div>
-        </div>
-    </div>
+              </div>
+          </div>
+          <div style="border-left:solid grey;"></div>
+          <div id="chat" class="container2">
+              <div class="text" id="frienname"></div>
+              <div style="height: 225px;overflow: auto;" id="ch">
+                Find and select your friends in leftside panel to view the conversation
+              </div>
+              <div class="container1">
+                  <input class="inp1" id="msg" type="text" placeholder="Write a message">
+                  <button class="btn-sec" id="send" onclick="send()">Send</button>
+              </div>
+          </div>
+      </div>
     `;
     document.title='Chat';
     friendlist();
 }
 function wrguser() {
     document.body.innerHTML=`
-    <div>
+    <div class="container">
         <h1>Invalid user id or password</h1>
-        <br/>
         <button class="btn-sec" onclick="welcome()">Back</button>
     </div>
     `;
@@ -67,13 +75,16 @@ function wrguser() {
 }
 function userexist() {
     document.body.innerHTML=`
-    <div>
+    <div class="container">
         <h1>User already exist</h1>
-        <br/>
         <button class="btn-sec" onclick="welcome()">Back</button>
     </div>
     `;
     document.title='User already exist';
+}
+
+function logout() {
+  window.location.href=url+`/chat/main.html`;
 }
 
 function log(){
@@ -89,7 +100,6 @@ function create(){
 
 function send(){
     var mes=document.getElementById('msg').value;
-    document.getElementById('msg').value='';
     var request=new XMLHttpRequest();
     request.onreadystatechange=function(){
         if(request.readyState===XMLHttpRequest.DONE){
@@ -103,7 +113,6 @@ function send(){
 }
 function search(){
     var friend=document.getElementById('friend').value;
-    document.getElementById('friend').value='';
     console.log(friend);
     var request=new XMLHttpRequest();
     request.onreadystatechange=function(){
@@ -177,13 +186,14 @@ function createfriendlist(resp){
 }
 
 function chatlist(fname){
+    document.getElementById('frienname').innerHTML=fname;
     var request=new XMLHttpRequest();
     request.onreadystatechange=function(){
         if(request.readyState===XMLHttpRequest.DONE){
             if(request.status===200){
                 var result=JSON.parse(request.responseText.trim());
                 var temp=`
-                    <table border="1px">
+                    <table style="width: 100%;">
                     `;
                 //generate chat list and return
                 for (var i = 0; i < result.rows.length; i++) {
@@ -191,7 +201,7 @@ function chatlist(fname){
                     if (temp1.flag===0) {//reciev
                         temp+=`
                             <tr>
-                            <td>`+temp1.msg+`</td>
+                            <td span class="green">`+temp1.msg+`</td>
                             <td></td>
                             </tr>
                             `;
@@ -199,12 +209,14 @@ function chatlist(fname){
                         temp+=`
                             <tr>
                             <td></td>
-                            <td>`+temp1.msg+`</td>
+                            <td span class="yellow">`+temp1.msg+`</td>
                             </tr>
                             `;
                     }
                 }
                 temp+=`</table>`;
+                if(result.rows.length===0)
+                temp=`No history of conversation`;
                 document.getElementById('ch').innerHTML=temp;
             }
         }
@@ -222,6 +234,7 @@ function friendlist() {
                 console.log(result.length);
                 if (result==='nonvalid') {
                     console.log("nonvalid");
+                    document.getElementById('fr').innerHTML="Find your friends using their username";
                 }else {
                     result=JSON.parse(result);
                     var temp='';
