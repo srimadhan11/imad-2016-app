fc6ad8e2d32f48db2081d2db2c487290a2c7b6f4
srimadhan11 <srimadhan11@gmail.com>
2016-11-20 21:03:42 +0530

    [imad-console] Updates newChat/lscript.js

--- newChat/lscript.js
+++ newChat/lscript.js
@@ -6,6 +6,19 @@ function login(){
     
     var request=new XMLHttpRequest();
     
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            if(request.status===200){
+                var result=request.responseText;
+                if(result.trim()==="sucess"){
+                    //redirect
+                }else{
+                    //notify
+                }
+            }
+        }
+    }
+    
     request.open('POST','http://srimadhan11.imad.hasura-app.io/new/login',true);
     request.setRequestHeader('Content-Type','application/json');
     request.send(JSON.stringify({username:username, passwword:password}));
@@ -15,6 +28,19 @@ function signup(){
     var password=document.getElementById('');
     
     var request=new XMLHttpRequest();
+    
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            if(request.status===200){
+                var result=request.responseText;
+                if(result.trim()==="valid"){
+                    //notify
+                }else{
+                    //notify
+                }
+            }
+        }
+    }
     
     request.open('POST','http://srimadhan11.imad.hasura-app.io/new/create',true);
     request.setRequestHeader('Content-Type','application/json');
