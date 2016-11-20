7107b4c731668aad1c684dc094a9de79ffac48dd
srimadhan11 <srimadhan11@gmail.com>
2016-11-20 20:56:26 +0530

    [imad-console] Updates newChat/lscript.js

--- newChat/lscript.js
+++ newChat/lscript.js
@@ -1,8 +1,22 @@
 var notify=document.getElementById('notification');
 
 function login(){
+    var username=document.getElementById('');
+    var password=document.getElementById('');
     
+    var request=new XMLHttpRequest();
+    
+    request.open('POST','http://srimadhan11.imad.hasura-app.io/new/login',true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username:username, passwword:password}));
 }
 function signup(){
+    var username=document.getElementById('');
+    var password=document.getElementById('');
     
+    var request=new XMLHttpRequest();
+    
+    request.open('POST','http://srimadhan11.imad.hasura-app.io/new/create',true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username:username, passwword:password}));
 }
