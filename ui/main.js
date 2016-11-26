ee8c6cb8b5168ba54e22ed7d5bfa2ff30ba4b795
srimadhan11 <srimadhan11@gmail.com>
2016-11-26 23:58:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -170,8 +170,10 @@ function submit(){
     request.onreadystatechange=function(){
         if(request.readyState===XMLHttpRequest.DONE){
             if(request.status===200){
-                var result=JSON.parse(request.responseText);
-                document.getElementById('commenttext').value='';
+                if(request.responseText.trim()==='sucess'){
+                    document.getElementById('commenttext').value='';
+                    comment();
+                }
             }
         }
     }
